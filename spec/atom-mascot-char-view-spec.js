a0d577d68ad9a22646c5ae91844288d683bf8a98
f0rk <dummy.github.1945@gmail.com>
2020-05-27 14:50:42 +0900

    Update

---
+++ spec/atom-mascot-char-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMascotCharView from '../lib/atom-mascot-char-view';
+
+describe('AtomMascotCharView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
